59ec6d3530fc8850e09851275466148aaa52fa32
michaelarayabe <michaelseifuaraya@gmail.com>
2021-01-19 08:48:59 +0100

    spring external properties

---
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -0,0 +1,21 @@
+package guru.springframework;
+
+import guru.springframework.controllers.MyController;
+import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUser());
+	}
+}
